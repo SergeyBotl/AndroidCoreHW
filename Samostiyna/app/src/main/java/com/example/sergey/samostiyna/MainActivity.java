aa37f2867f2bc585e40ee65a6c783e4a2e438621
Sergey <bot74@mail.ru>
2016-12-01 23:17:06 +0200

    add

--- Samostiyna/app/src/main/java/com/example/sergey/samostiyna/MainActivity.java
+++ Samostiyna/app/src/main/java/com/example/sergey/samostiyna/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -24,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         editText1 = (EditText) findViewById(R.id.editText);
         editText2 = (EditText) findViewById(R.id.editText2);
         textView = (TextView) findViewById(R.id.textView);
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Thickness t;
-                if (editText3.getText().toString().equals(thick)) {
+                if (editText3.getText().toString().equals("thick")) {
                     t = thick;
                 } else {
                     t = thin;
