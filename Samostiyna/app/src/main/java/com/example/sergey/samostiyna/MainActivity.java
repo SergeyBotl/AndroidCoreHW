fe57f32d9f846f23c44bb14206e37dd2fb74c02a
Sergey <bot74@mail.ru>
2016-12-02 13:32:55 +0200

    add

--- Samostiyna/app/src/main/java/com/example/sergey/samostiyna/MainActivity.java
+++ Samostiyna/app/src/main/java/com/example/sergey/samostiyna/MainActivity.java
@@ -2,20 +2,25 @@ package com.example.sergey.samostiyna;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static com.example.sergey.samostiyna.Thickness.*;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
-    private EditText editText1, editText2, editText3;
+    private EditText editText1, editText2;
     private List<Pizza> list = new ArrayList<>();
+    private List<String> thicknesses = new ArrayList<>(Arrays.asList("thick", "thin"));
+    private Spinner spinner;
+    private ArrayAdapter<String> adapter;
 
 
     @Override
@@ -25,8 +30,12 @@ public class MainActivity extends AppCompatActivity {
 
         editText1 = (EditText) findViewById(R.id.editText);
         editText2 = (EditText) findViewById(R.id.editText2);
-        editText3 = (EditText) findViewById(R.id.editText3);
         textView = (TextView) findViewById(R.id.textView);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, thicknesses);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         Button buttonSave = (Button) findViewById(R.id.buttonSave);
         Button buttonRead = (Button) findViewById(R.id.buttonRead);
@@ -35,12 +44,9 @@ public class MainActivity extends AppCompatActivity {
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Thickness t;
-                if (editText3.getText().toString().equals("thick")) {
-                    t = thick;
-                } else {
-                    t = thin;
-                }
+
+                Thickness t = Thickness.valueOf(spinner.getSelectedItem().toString());
+                Log.i("TAG","ID "+spinner.getSelectedItemId()+" Position "+spinner.getSelectedItemPosition());
                 list.add(new Pizza(editText1.getText().toString(),
                         Float.valueOf(editText2.getText().toString()),
                         t));
@@ -50,8 +56,8 @@ public class MainActivity extends AppCompatActivity {
         buttonRead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (list!=null)
-                textView.setText(list.toString());
+                if (list != null)
+                    textView.setText(list.toString());
             }
         });
     }
