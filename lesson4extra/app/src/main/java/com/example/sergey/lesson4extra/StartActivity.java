8d3ade49e8221b03efe77d97b038a1412c8d7685
Sergey <bot74@mail.ru>
2016-12-06 10:21:01 +0200

    add

---
+++ lesson4extra/app/src/main/java/com/example/sergey/lesson4extra/StartActivity.java
@@ -0,0 +1,33 @@
+package com.example.sergey.lesson4extra;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    CurrentUser currentUser = new CurrentUser();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button button = (Button) findViewById(R.id.buttonStart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (currentUser.getUserRegistry() == null) {
+                    Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(StartActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+}
