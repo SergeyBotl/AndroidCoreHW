572a277f867d869802aae73d339b6a8c6874ec48
Sergey <bot74@mail.ru>
2016-12-08 16:57:57 +0200

    add

--- lesson3_extra2/app/src/main/java/com/example/sergey/lesson3_extra2/MainActivity.java
+++ lesson3_extra2/app/src/main/java/com/example/sergey/lesson3_extra2/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.sergey.lesson3_extra2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+List<?>list=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        adapter.onI
     }
 }
