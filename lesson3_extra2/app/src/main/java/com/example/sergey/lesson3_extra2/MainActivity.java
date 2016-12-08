131e54d6c088c2e18f00cd3117b8ca4328af745c
Sergey <bot74@mail.ru>
2016-12-08 17:59:58 +0200

    add

--- lesson3_extra2/app/src/main/java/com/example/sergey/lesson3_extra2/MainActivity.java
+++ lesson3_extra2/app/src/main/java/com/example/sergey/lesson3_extra2/MainActivity.java
@@ -2,27 +2,32 @@ package com.example.sergey.lesson3_extra2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-List<?>list=new ArrayList<>();
+
+    String[] s = {"a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x"};
+    List<?> list = new ArrayList<>(Arrays.asList(s));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView=(ListView)findViewById(R.id.listView);
-        ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
         listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+                Log.i("tag","int "+i+" long "+l);
             }
 
             @Override
