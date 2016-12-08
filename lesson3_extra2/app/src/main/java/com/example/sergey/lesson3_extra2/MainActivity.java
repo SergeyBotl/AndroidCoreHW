7d824db1e8f459e3716937407fec620b52c7a03f
Sergey <bot74@mail.ru>
2016-12-08 17:52:44 +0200

    add

--- lesson3_extra2/app/src/main/java/com/example/sergey/lesson3_extra2/MainActivity.java
+++ lesson3_extra2/app/src/main/java/com/example/sergey/lesson3_extra2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sergey.lesson3_extra2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +16,19 @@ List<?>list=new ArrayList<>();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ListView listView=(ListView)findViewById(R.id.listView);
         ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
-        adapter.onI
+        listView.setAdapter(adapter);
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
