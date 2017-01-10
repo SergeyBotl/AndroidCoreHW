9a68e87254d090cb7ae884661cc0fa74fc7fdce3
Sergey <bot74@mail.ru>
2017-01-10 02:11:21 +0200

    add

---
+++ lesson8_image_webview/app/src/main/java/com/example/sergey/lesson8_image_webview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sergey.lesson8_image_webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
